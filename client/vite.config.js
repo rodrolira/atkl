8a5622c62b8dc52b5706ccb892b4826c1257d75a
Rodrigo Lira <rlirapizarro@gmail.com>
2024-09-15 09:38:52 -0300

    Fix: Navbar styling inconsistencies on different screen sizes
    The changes address styling inconsistencies in the navbar across various screen sizes.
    
    - The navbar's padding on medium screens is corrected to ensure consistent spacing.
    - The vite configuration is updated to enable local network hosting for development purposes.

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,9 @@ import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true
+  },
   build: {
     sourcemap: false,
     rollupOptions: {
