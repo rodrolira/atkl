8d2581bb525180846a611f8bad93e1ca6be4767c
Rodrigo Lira <rlirapizarro@gmail.com>
2024-09-26 18:32:16 -0300

    feat: Implement language selection
    Add support for language switching, allowing users to choose their preferred language.
    
    - Implement language selection menu
    - Allow users to change language settings in the footer
    - Store language preference in local storage
    
    Resolves #22

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
 import Routing from './routes/Routing';
+import { useTranslation } from 'react-i18next'
 import './App.css'
 
 import './i18n'
 import Footer from './components/Footer/Footer';
 
 function App() {
+  const { i18n } = useTranslation()
+
+  useEffect(() => {
+    const savedLanguage = localStorage.getItem('language')
+    if (savedLanguage) {
+      i18n.changeLanguage(savedLanguage)
+    }
+  }, [i18n])
+  
   return (
     <div id='app' className='flex'>
       <div className='layout'>
--- client/src/components/Language/LanguageMenu.jsx
+++ client/src/components/Language/LanguageMenu.jsx
@@ -1,16 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactCountryFlag from 'react-country-flag'
 import { useTranslation } from 'react-i18next'
 import { useLanguage } from '@/hooks/useLanguage'
+
 const LanguageMenu = () => {
   const { t, i18n } = useTranslation()
   const { changeLanguage, language } = useLanguage()
   const [isMenuVisible, setMenuVisibility] = useState(false)
   const [selectedLanguage, setSelectedLanguage] = useState(language)
 
-  const handleChangeLanguage = languageCode => {
+  useEffect(() => {
+    const savedLanguage = localStorage.getItem('language')
+    if (savedLanguage) {
+      i18n.changeLanguage(savedLanguage)
+      setSelectedLanguage(savedLanguage)
+    }
+  }, [i18n])
+  const handleChangeLanguage = (languageCode) => {
     changeLanguage(languageCode)
     i18n.changeLanguage(languageCode) // Cambiar idioma en i18next
+    localStorage.setItem('language', languageCode) // Guardar en localStorage
     setSelectedLanguage(languageCode)
     setMenuVisibility(false)
   }
