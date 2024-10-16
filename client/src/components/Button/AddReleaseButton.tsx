c46ba7d72e745d841f19cb59098aedb96b4b883c
Rodrigo Lira <rlirapizarro@gmail.com>
2024-10-15 23:44:07 -0300

    feat: Add new button components
    Adds several new button components for the admin panel:
    
    - AddArtistButton
    - AddReleaseButton
    - AdminLogoutButton
    - DemoButton
    
    These buttons will improve the functionality and user experience for admin users.

---
+++ client/src/components/Button/AddReleaseButton.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import AddReleaseForm from '@/components/Release/AddRelease/AddReleaseForm';
+import Button from './Button';
+import { useTranslation } from 'react-i18next';
+
+interface AddReleaseButtonProps {
+  className?: string;
+  openPopup?: boolean;
+  closePopup?: () => void;
+  onReleaseAdded?: () => void;
+  children?: React.ReactNode; // Add this line
+}
+
+const AddReleaseButton: React.FC<AddReleaseButtonProps> = ({
+  className,
+  openPopup,
+  onReleaseAdded,
+  children,
+}) => {
+
+
+  const { t } = useTranslation();
+  const [open, openChange] = useState<boolean>(false);
+
+  const functionOpenPopup = () => {
+    openChange(true);
+  };
+
+  const closePopup = () => {
+    openChange(false);
+  };
+
+  return (
+    <>
+      <Button
+        onClick={functionOpenPopup}
+        fontWeight="font-bold"
+        colorClass="bg-[#24db13] text-[#051403]"
+      >
+        {t('add_release')}
+      </Button>
+
+      <AddReleaseForm open={open} closePopup={closePopup} onReleaseAdded={(newRelease: any) => { }} />
+    </>
+  );
+};
+
+
+
+
+export default AddReleaseButton;
---
+++ client/src/components/Button/CircularButtons.tsx
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import IconButton from '@mui/material/IconButton';
+import PersonAddIcon from '@mui/icons-material/PersonAdd'; // Icon for adding artist
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic'; // Icon for adding release
+import ExitToAppIcon from '@mui/icons-material/ExitToApp'; // Icon for logout
+import './CircularButtons.css'; // Import styles for circular buttons
+import AddArtistForm from '@/components/Artist/AddArtistForm';
+import AddReleaseForm from '@/components/Release/AddRelease/AddReleaseForm'; // Import form
+import { useNavigate } from 'react-router-dom';
+import { useAdminAuth } from '@/contexts/AdminAuthContext';
+import { useTranslation } from 'react-i18next';
+import { Artist } from '../../types/interfaces/Artist';
+
+// Type definitions for props or event handling if necessary
+interface ArtistProps {
+  artist: Artist;
+}
+
+// Button to add artist
+export const AddArtistButton: React.FC = () => {
+  const [open, setOpen] = useState<boolean>(false);
+  const { t } = useTranslation();
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+  const { isAuthenticated: adminAuthenticated } = useAdminAuth();
+
+  const handleOpen = () => setIsDialogOpen(true);
+  const handleClose = () => setIsDialogOpen(false);
+
+  const openPopup = () => setOpen(true);
+  const closePopup = () => setOpen(false);
+
+  return (
+    <>
+      {/* Circular Button */}
+      {adminAuthenticated && (
+        <IconButton onClick={handleOpen} className="circular-button">
+          <PersonAddIcon fontSize="large" className="button-icon" />
+          {/* Form popup */}
+        </IconButton>
+      )}
+
+      {/* Form to add artist */}
+      {isDialogOpen && (
+        <AddArtistForm
+          openPopup={isDialogOpen}
+          closePopup={handleClose}
+          onArtistAdded={(newArtist: Artist) => {
+            console.log('New artist added:', newArtist);
+          }}
+        />
+      )}
+    </>
+  );
+};
+
+// Button to add release
+export const AddReleaseButton: React.FC = () => {
+  const [open, setOpen] = useState<boolean>(false);
+  const { isAuthenticated: adminAuthenticated } = useAdminAuth();
+
+  const functionOpenPopup = () => {
+    setOpen(true);
+  };
+
+  const closePopup = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      {/* Circular Button */}
+      {adminAuthenticated && (
+        <IconButton onClick={functionOpenPopup} className="circular-button">
+          <LibraryMusicIcon fontSize="large" className="button-icon" />
+        </IconButton>
+      )}
+
+      {/* Form popup */}
+      <AddReleaseForm open={open} closePopup={closePopup} onReleaseAdded={(newRelease: any) => {}} />
+    </>
+  );
+};
+
+// Button for logout
+export const AdminLogoutButton: React.FC = () => {
+  const { signout, isAuthenticated: adminAuthenticated } = useAdminAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await signout(); // Call your logout function
+    navigate('/');
+  };
+
+  return (
+    <>
+      {adminAuthenticated && (
+        <IconButton onClick={handleLogout} className="circular-button">
+          <ExitToAppIcon fontSize="large" className="button-icon" />
+        </IconButton>
+      )}
+    </>
+  );
+};
