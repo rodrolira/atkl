6377098de836c31df8e8a19e713f47969726b0a5
Rodrigo Lira <rlirapizarro@gmail.com>
2024-11-07 04:59:55 -0300

    Fix: Update build directory path
    The build directory path was incorrectly set to `build`, which resulted in the application failing to load static files. This commit updates the path to `dist`, resolving the issue.

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -13,19 +13,7 @@ const baseQuery = fetchBaseQuery({
 });
 
 const baseQueryWithReauth = async (args, api, extraOptions) => {
-  const result = await baseQuery(args, api, extraOptions);
-  // ====================== TODO: Implement 401 unauthorized re-authentication ======================
-  // if (result?.error?.status === 401) {
-  //   const refreshResult = await baseQuery('/refresh', api, extraOptions);
-  //   if (refreshResult?.data) {
-  //     const { user } = api.getState().auth;
-  //     api.dispatch(setCredentials({ user, ...refreshResult.data }));
-  //     result = await baseQuery(args, api, extraOptions);
-  //   } else {
-  //     api.dispatch(logOut());
-  //   }
-  // }
-  return result;
+  return await baseQuery(args, api, extraOptions);
 };
 
 const apiSlice = createApi({
