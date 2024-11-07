b9fae629aed0fe9cb349d3aa701714c79d590552
Rodrigo Lira <rlirapizarro@gmail.com>
2024-11-07 00:26:34 -0300

    feat: Add Vercel analytics
    Integrate Vercel Analytics for tracking website usage. This will provide insights into website traffic, user behavior, and overall performance.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
 import Routing from './routes/Routing';
 import './App.css';
+import { Analytics } from "@vercel/analytics/react"
 
 import './i18n';
 import Footer from './components/Footer/Footer';
